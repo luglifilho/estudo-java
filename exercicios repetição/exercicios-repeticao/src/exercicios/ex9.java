6c9a9a0bc8c955ec0137d0d140341a25e9979156
luglifilho <luglifilho@hotmail.com>
2023-01-12 21:36:42 -0300

    desafios dio

---
+++ exercicios repetição/exercicios-repeticao/src/exercicios/ex9.java
@@ -0,0 +1,18 @@
+package exercicios;
+
+import java.util.Scanner;
+
+public class ex9 {
+
+    public static void main(String[] args) {
+        Scanner leitor = new Scanner(System.in);
+        int entrada = leitor.nextInt();
+        int mesada = 50;
+
+        // TODO: Crie a condição necessária para que o sobrinho saiba quanto dinheiro
+        // irá juntar nos meses da entrada
+
+        System.out.println(entrada * mesada);
+
+    }
+}
